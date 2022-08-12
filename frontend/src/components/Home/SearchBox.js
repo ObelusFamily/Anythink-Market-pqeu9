57f2fcc186f476a47f9232ddee4d7cff36701d66
Matthew C Wake <mcwake-dev@outlook.com>
2022-08-12 02:30:27 +0100

    Moved title to search by state into itemsList reducer

--- frontend/src/components/Home/SearchBox.js
+++ frontend/src/components/Home/SearchBox.js
@@ -8,69 +8,61 @@ const mapDispatchToProps = (dispatch) => ({
     dispatch({ type: APPLY_TITLE_FILTER, title, pager, payload }),
 });
 
-class SearchBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.title = "";
-  }
+const mapStateToProps = (state) => ({
+  title: state.itemList.title,
+  items: state.itemList,
+});
 
-  searchTextChange = (ev) => {
-    const title = ev.currentTarget.value;
+const SearchBox = ({ title, onSearchText }) => {
+  const searchTextChange = (ev) => {
+    const newTitle = ev.currentTarget.value;
 
-    this.setState({ title });
-
-    console.log(title.length);
-
-    if (title.length === 0) {
-      this.props.onSearchText(
-        "",
-        (page) => agent.Items.all(title, page),
-        agent.Items.all(title)
+    if (newTitle.length < 3) {
+      onSearchText(
+        newTitle,
+        (page) => agent.Items.all(page),
+        agent.Items.all()
       );
-    }
-
-    if (title.length > 2) {
-      this.props.onSearchText(
-        title,
-        (page) => agent.Items.byTitle(title, page),
-        agent.Items.byTitle(title)
+    } else {
+      onSearchText(
+        newTitle,
+        (page) => agent.Items.byTitle(newTitle, page),
+        agent.Items.byTitle(newTitle)
       );
     }
   };
 
-  render() {
-    return (
-      <div className="form-group form-group-lg">
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <span
-              id="get-part"
-              className="input-group-text text-white bg-transparent border-0"
-            >
-              A place to get
-            </span>
-          </div>
-          <input
-            id="search-box"
-            className="form-control border-right-0 rounded-left bg-white"
-            type="text"
-            placeholder="What is it that you truly desire?"
-            value={this.searchText}
-            onChange={this.searchTextChange}
-          />
-          <div className="input-group-text bg-white border-left-0 rounded-right">
-            <i className="bi bi-search"></i>
-          </div>
-          <div className="input-group-append">
-            <span className="input-group-text text-white border-0 bg-transparent">
-              {" "}
-              the cool stuff.
-            </span>
-          </div>
+  return (
+    <div className="form-group form-group-lg">
+      <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <span
+            id="get-part"
+            className="input-group-text text-white bg-transparent border-0"
+          >
+            A place to get
+          </span>
+        </div>
+        <input
+          id="search-box"
+          className="form-control border-right-0 rounded-left bg-white"
+          type="text"
+          placeholder="What is it that you truly desire?"
+          value={title}
+          onChange={searchTextChange}
+        />
+        <div className="input-group-text bg-white border-left-0 rounded-right">
+          <i className="bi bi-search"></i>
+        </div>
+        <div className="input-group-append">
+          <span className="input-group-text text-white border-0 bg-transparent">
+            {" "}
+            the cool stuff.
+          </span>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default connect(null, mapDispatchToProps)(SearchBox);
+export default connect(mapStateToProps, mapDispatchToProps)(SearchBox);
